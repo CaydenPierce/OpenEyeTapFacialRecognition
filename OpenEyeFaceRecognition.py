65365061e7db425900fd99ed5721a3833a2c763e
CaydenPierce <38359347+CaydenPierce@users.noreply.github.com>
2019-01-11 11:04:53 -0500

    cleaning up

--- OpenEyeFaceRecognition.py
+++
@@ -1,7 +0,0 @@
-import face_recognition
-
-image = face_recognition.load_image_file("Cayden_Pierce.jpg")
-
-list_of_face_encodings = face_recognition.face_encodings(image)
-
-print(list_of_face_encodings[0])
